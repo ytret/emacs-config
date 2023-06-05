9f2a0510973256b1c780e5ceb83ef112a7fa4903
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-06-05 12:28:13 +0300

    init.el: install paredit

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(avy magit which-key undo-tree vertico)))
+ '(package-selected-packages '(paredit avy magit which-key undo-tree vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
