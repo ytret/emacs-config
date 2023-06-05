3f081e3d803f5591328527577a83266fe69fd378
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-06-05 13:29:29 +0300

    10-editing.el: enable paredit-mode for Lisps

--- config/10-editing.el
+++ config/10-editing.el
@@ -12,6 +12,9 @@
 ;;; Show column number.
 (column-number-mode)
 
+;;; Save place in each file.
+(save-place-mode)
+
 ;;; Whitespace rules.
 (add-hook 'before-save-hook #'delete-trailing-whitespace)
 (setq require-final-newline t)
@@ -21,12 +24,18 @@
 (my-add-to-hooks (lambda () (setq truncate-lines t))
 		 '(prog-mode-hook Info-mode-hook))
 
-;;; Save place in each file.
-(save-place-mode)
-
 ;;; Set fill column to 80 in some modes.
 (my-add-to-hooks (lambda () (setq fill-column 80))
 		 '(prog-mode-hook text-mode-hook))
+
+;; (require 'paredit)
+(my-add-to-hooks #'enable-paredit-mode
+		 '(emacs-lisp-mode-hook
+		   eval-expression-minibuffer-setup-hook
+		   ielm-mode-hook
+		   lisp-mode-hook
+		   lisp-interaction-mode-hook
+		   scheme-mode-hook))
 
 ;;; Mark characters past the 80th column.
 (require 'whitespace)
