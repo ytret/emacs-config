c2425e4af4ce417afd0e4c4c91435e1edf16e5d1
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-06-10 16:45:50 +0300

    11-ui.el: fix default face height

--- config/11-ui.el
+++ config/11-ui.el
@@ -1,9 +1,11 @@
 ;;; Emacs UI configuration.
 
 (setq default-frame-alist `((left . 53) (top . 135)
-			    (width . 80) (height . 44)
-			    (font . "Hack 11")))
+			    (width . 80) (height . 44)))
 
+(set-face-attribute 'default nil
+		    :family "Hack"
+		    :height 105)
 (set-face-attribute 'fixed-pitch-serif nil
 		    :family "Hack"
 		    :weight 'bold)
