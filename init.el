675c6d1e4b7cd9574c6d3c63c7eac466ab499d95
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-08-01 12:04:16 +0300

    init.el: установлен пакет elpy

--- init.el
+++ init.el
@@ -13,7 +13,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(lsp-mode paredit avy magit which-key undo-tree vertico)))
+ '(package-selected-packages
+   '(elpy lsp-mode paredit avy magit which-key undo-tree vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
