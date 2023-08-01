7fd5f42bc9968c76e830cf671f1bdc32382bbc8f
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-08-01 12:25:12 +0300

    11-ui.el: вкл. отображение номеров строк в prog-modes

--- config/11-ui.el
+++ config/11-ui.el
@@ -1,7 +1,7 @@
 ;;; Emacs UI configuration.
 
 (setq default-frame-alist `((left . 1170) (top . 135)
-			    (width . 80) (height . 44)
+			    (width . 84) (height . 44)
 			    (font . "Hack 11")))
 
 (set-face-attribute 'fixed-pitch-serif nil
@@ -11,3 +11,5 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
