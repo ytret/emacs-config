33829475673ded62d963afabe3c3406fa18fe934
Yuri Tretyakov <ytretyakov18@gmail.com>
2023-08-02 11:24:08 +0300

    Файлы автосохранения помещает в ~/.cache

--- config/18-backup.el
+++ config/18-backup.el
@@ -2,6 +2,7 @@
 
 (setq backup-by-copying t
       backup-directory-alist '(("." . "~/.cache/emacs/backups"))
+      auto-save-file-name-transforms `(("." "~/.cache/emacs/backups" t))
       delete-old-versions t
       keep-new-versions 6
       keep-old-versions 2
